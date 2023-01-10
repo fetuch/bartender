eff1f9d01875132fd602d3ad5b486618ae75fc33
Fetuch <rafal.nowak@punkgate.pl>
2023-01-10 20:39:52 +0100

    Use search fontawesome icon

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex h-full items-center px-8">
+      <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
       <span><span class="text-brand-green-1">69</span> drinks matched</span>
     </div>
   </div>
