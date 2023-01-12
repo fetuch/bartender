93e5d6cf0db8b9d5b9dc3519e4dea6174a7939b9
Fetuch <rafal.nowak@punkgate.pl>
2023-01-12 11:20:12 +0100

    Dynamicly adjust main nav height

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
@@ -46,10 +46,19 @@ export default {
   data() {
     return {
       appName: "Bartender App",
-      url: "https://careers.google.com",
+      url: "/",
       menuItems: ["Drinks", "Ingredients"],
       isLoggedIn: false,
     };
+  },
+
+  computed: {
+    headerHeightClass() {
+      return {
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
+      };
+    },
   },
 
   methods: {
