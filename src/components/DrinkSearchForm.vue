b9538f3cf21ae01ae6b04f386014a8dbf1eb0f59
Fetuch <rafal.nowak@punkgate.pl>
2023-01-15 18:54:36 +0100

    Add drink form component

---
+++ src/components/DrinkSearchForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form
+    class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light">
+      <div class="relative flex h-full flex-1 items-center pr-3">
+        <label class="absolute left-0 -top-10">Ingredients</label>
+        <input
+          v-model="ingredients"
+          type="text"
+          placeholder="Gin, rum, orange juice, banana..."
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+    </div>
+
+    <action-button text="Search" type="secondary" class="rounded-r-3xl" />
+  </form>
+</template>
+
+<script>
+import ActionButton from "@/components/ActionButton.vue";
+
+export default {
+  name: "DrinkSearchForm",
+  components: { ActionButton },
+  data() {
+    return {
+      ingredients: "",
+    };
+  },
+};
+</script>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,22 +1,18 @@
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
+    <section class="flex h-screen flex-col pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-span-1 col-start-1 border border-blue-700">
-          1 column
-        </div>
+        <div class="col-span-1 col-start-1"></div>
 
-        <div class="col-span-5 col-start-2 border border-blue-700">
+        <div class="col-span-5 col-start-2">
           <the-headline />
+
+          <drink-search-form />
         </div>
 
-        <div class="col-span-5 col-start-7 border border-blue-700">
-          5 columns
-        </div>
+        <div class="col-span-5 col-start-7"></div>
 
-        <div class="col-span-1 col-start-12 border border-blue-700">
-          1 column
-        </div>
+        <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
   </main>
@@ -24,12 +20,14 @@
 
 <script>
 import TheHeadline from "@/components/TheHeadline.vue";
+import DrinkSearchForm from "@/components/DrinkSearchForm.vue";
 
 export default {
   name: "TheHero",
 
   components: {
     TheHeadline,
+    DrinkSearchForm,
   },
 };
 </script>
