ec2257f990a1faa20dba55c77ecbf00e2c3a8c82
Fetuch <rafal.nowak@punkgate.pl>
2023-01-16 10:36:57 +0100

    use text input component in drink search form

--- src/components/DrinkSearch/DrinkSearchForm.vue
+++ src/components/DrinkSearch/DrinkSearchForm.vue
@@ -7,11 +7,10 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute left-0 -top-10">Ingredients</label>
-        <input
+
+        <text-input
           v-model="ingredients"
-          type="text"
           placeholder="Gin, rum, orange juice, banana..."
-          class="w-full text-lg font-normal focus:outline-none"
         />
       </div>
     </div>
@@ -22,10 +21,11 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 
 export default {
   name: "DrinkSearchForm",
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data() {
     return {
       ingredients: "",
