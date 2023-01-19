0f0ba205f8b130c5fbc961fb5557196fd5cf2289
Fetuch <rafal.nowak@punkgate.pl>
2023-01-19 13:38:48 +0100

    Update drink listing to render drink data

--- src/components/DrinkResults/DrinkListing.vue
+++ src/components/DrinkResults/DrinkListing.vue
@@ -1,46 +1,41 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/drinks/1"
+      :to="drinkPageLink"
       class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
-        <h2 class="mb-2 text-2xl">Mojito</h2>
+        <h2 class="mb-2 text-2xl">{{ drink.name }}</h2>
 
         <div class="mb-3">
-          <span>Rum, Lime Juice, Mint Leaves</span>
-        </div>
-
-        <div>
-          <span>
-            There are different theories on who invented the mojito. Some
-            believe it was created in the 19th century by African slaves who
-            worked in the Cuban sugarcane fields. Others say that it was created
-            by Naval Officer Sir Francis Drake while he was in Cuba. What we do
-            know is that mojitos began to gain popularity in Havana, Cuba in the
-            1900s and it’s now one of the most popular cocktails in the world.
-          </span>
+          <span>{{ drink.category }}, {{ drink.glass }}</span>
         </div>
       </div>
 
       <div class="px-8 py-4">
         <div>
-          <h3 class="mt-1 mb-2">HOW TO MAKE A MOJITO:</h3>
+          <h3 class="mt-1 mb-2 uppercase">How to make {{ drink.name }}</h3>
+
+          <div class="mb-5">
+            <span>
+              {{ drink.instructions }}
+            </span>
+          </div>
+
+          <h3 class="mt-1 mb-2 uppercase">Ingredients</h3>
+
           <div>
             <ul class="list-disc pl-8">
-              <li>2 ounces of white rum</li>
-              <li>½ ounce of lime juice</li>
-              <li>1 teaspoon of sugar</li>
-              <li>3 mint leaves</li>
-              <li>Cracked ice</li>
-              <li>Club soda or seltzer</li>
-              <li>½ ounce of simple syrup</li>
+              <li v-for="ingredient in drink.ingredients" :key="ingredient">
+                {{ ingredient.name }}<span v-show="ingredient.measure">,</span>
+                {{ ingredient.measure }}
+              </li>
             </ul>
           </div>
         </div>
 
         <div class="mt-2 text-center">
-          <router-link to="/drinks/1" class="text-brand-blue-1"
+          <router-link :to="drinkPageLink" class="text-brand-blue-1"
             >Expand</router-link
           >
         </div>
@@ -52,5 +47,16 @@
 <script>
 export default {
   name: "DrinkListing",
+  props: {
+    drink: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    drinkPageLink() {
+      return `/drinks/${this.drink.id}`;
+    },
+  },
 };
 </script>
--- src/components/DrinkResults/DrinkListings.vue
+++ src/components/DrinkResults/DrinkListings.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <drink-listing v-for="drink in drinks" :key="drink.id" />
+      <drink-listing v-for="drink in drinks" :key="drink.id" :drink="drink" />
     </ol>
   </main>
 </template>
