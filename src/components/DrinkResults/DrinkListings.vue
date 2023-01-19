0e02108f8e6d5f47573e4d216112990c04ef022c
Fetuch <rafal.nowak@punkgate.pl>
2023-01-19 11:23:53 +0100

    Use axios to fetch drinks list

--- src/components/DrinkResults/DrinkListings.vue
+++ src/components/DrinkResults/DrinkListings.vue
@@ -1,18 +1,27 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <drink-listing />
-      <drink-listing />
-      <drink-listing />
+      <drink-listing v-for="drink in drinks" :key="drink.id" />
     </ol>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+
 import DrinkListing from "@/components/DrinkResults/DrinkListing.vue";
 
 export default {
   name: "DrinkListings",
   components: { DrinkListing },
+  data() {
+    return {
+      drinks: [],
+    };
+  },
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/drinks");
+    this.drinks = response.data;
+  },
 };
 </script>
