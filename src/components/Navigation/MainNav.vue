a6af9b5654bab4f78c2335c6fd5701e1a97de64d
Fetuch <rafal.nowak@punkgate.pl>
2023-01-20 12:47:46 +0100

    Use user store in main nav component

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -38,6 +38,10 @@
 </template>
 
 <script>
+import { mapActions, mapState } from "pinia";
+
+import { useUserStore } from "@/stores/user";
+
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import ProfileImage from "@/components/Navigation/ProfileImage.vue";
 import TheSubnav from "@/components/Navigation/TheSubnav.vue";
@@ -57,11 +61,11 @@ export default {
         { text: "Drinks", name: "DrinkResults" },
         { text: "Ingredients", name: "Home" },
       ],
-      isLoggedIn: false,
     };
   },
 
   computed: {
+    ...mapState(useUserStore, ["isLoggedIn"]),
     headerHeightClass() {
       return {
         "h-16": !this.isLoggedIn,
@@ -71,9 +75,7 @@ export default {
   },
 
   methods: {
-    loginUser() {
-      this.isLoggedIn = true;
-    },
+    ...mapActions(useUserStore, ["loginUser"]),
   },
 };
 </script>
