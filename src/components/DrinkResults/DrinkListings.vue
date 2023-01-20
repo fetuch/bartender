568fa8c778433bfbf6d6719b8d9e3b1c61896745
Fetuch <rafal.nowak@punkgate.pl>
2023-01-20 10:45:04 +0100

    Add pagination to drink listings

--- src/components/DrinkResults/DrinkListings.vue
+++ src/components/DrinkResults/DrinkListings.vue
@@ -1,8 +1,38 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <drink-listing v-for="drink in drinks" :key="drink.id" :drink="drink" />
+      <drink-listing
+        v-for="drink in displayedDrinks"
+        :key="drink.id"
+        :drink="drink"
+      />
     </ol>
+
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            role="link"
+            :to="{ name: 'DrinkResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            Previous
+          </router-link>
+
+          <router-link
+            v-if="nextPage"
+            role="link"
+            :to="{ name: 'DrinkResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            Next
+          </router-link>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -17,7 +47,29 @@ export default {
   data() {
     return {
       drinks: [],
+      perPage: 10,
     };
+  },
+  computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || "1");
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      const firstPage = 1;
+      return previousPage >= firstPage ? previousPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = Math.ceil(this.drinks.length / this.perPage);
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
+    displayedDrinks() {
+      const pageNumber = this.currentPage;
+      const firstDrinkIndex = (pageNumber - 1) * this.perPage;
+      const lastDrinkIndex = pageNumber * this.perPage;
+      return this.drinks.slice(firstDrinkIndex, lastDrinkIndex);
+    },
   },
   async mounted() {
     const response = await axios.get("http://localhost:3000/drinks");
--- tests/unit/components/DrinkResults/DrinkListings.test.js
+++ tests/unit/components/DrinkResults/DrinkListings.test.js
@@ -7,24 +7,43 @@ import DrinkListings from "@/components/DrinkResults/DrinkListings.vue";
 vi.mock("axios");
 
 describe("DrinkListings", () => {
-  it("fetches drinks", () => {
-    axios.get.mockResolvedValue({ data: [] });
-    render(DrinkListings);
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/drinks");
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5",
+      ...queryParams,
+    },
   });
 
-  it("creates a drink listing for every drink", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-
+  const renderDrinkListings = ($route) => {
     render(DrinkListings, {
       global: {
+        mocks: {
+          $route,
+        },
         stubs: {
           RouterLink: RouterLinkStub,
         },
       },
     });
+  };
+
+  it("fetches drinks", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const $route = createRoute();
+
+    renderDrinkListings($route);
+
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/drinks");
+  });
+
+  it("displays maximum of 10 drinks", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const queryParams = { page: "1" };
+    const $route = createRoute(queryParams);
+
+    renderDrinkListings($route);
 
     const drinkListings = await screen.findAllByRole("listitem");
-    expect(drinkListings).toHaveLength(15);
+    expect(drinkListings).toHaveLength(10);
   });
 });
