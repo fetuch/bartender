24bef41e5c50214b95c4a9168765283049832772
Fetuch <rafal.nowak@punkgate.pl>
2023-01-21 10:10:46 +0100

    Add spotlight component

--- src/components/DrinkSearch/TheHero.vue
+++ src/components/DrinkSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pt-10 pb-20">
+  <main class="flex h-screen flex-col">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
 
@@ -20,12 +20,38 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <router-link
+          to="/drinks"
+          class="mx-5 flex h-96 w-72 flex-col rounded-lg border bg-brand-gray-2"
+        >
+          <img :src="img" class="h-48 object-cover" />
+
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">
+              {{ title }}
+            </h3>
+
+            <p class="mt-3 text-sm">
+              {{ description }}
+            </p>
+          </div>
+
+          <router-link to="/drinks" class="px-6 pb-4 text-sm text-brand-blue-1"
+            >See drink</router-link
+          >
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import TheHeadline from "@/components/DrinkSearch/TheHeadline.vue";
 import DrinkSearchForm from "@/components/DrinkSearch/DrinkSearchForm.vue";
+import SpotLight from "@/components/DrinkSearch/SpotLight.vue";
 
 export default {
   name: "TheHero",
@@ -33,6 +59,7 @@ export default {
   components: {
     TheHeadline,
     DrinkSearchForm,
+    SpotLight,
   },
 };
 </script>
