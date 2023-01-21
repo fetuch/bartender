833a1b9cc78dcc259897e6b5673c31b4866781b2
Fetuch <rafal.nowak@punkgate.pl>
2023-01-21 07:04:50 +0100

    Use accordion component in filters sidebar

--- src/components/DrinkResults/DrinkFiltersSidebar/DrinkFiltersSidebar.vue
+++ src/components/DrinkResults/DrinkFiltersSidebar/DrinkFiltersSidebar.vue
@@ -2,12 +2,30 @@
   <div
     class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
   >
-    Drink Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">
+          What are you searching for?
+        </h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+
+      <collapsible-accordion header="Drink category" />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
 export default {
   name: "DrinkFiltersSidebar",
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+  },
 };
 </script>
