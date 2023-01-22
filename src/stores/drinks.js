7b0066907c011fa1946d6bd85860a9c559898508
Fetuch <rafal.nowak@punkgate.pl>
2023-01-22 09:04:55 +0100

    Add filtered drinks by categories getter

--- src/stores/drinks.js
+++ src/stores/drinks.js
@@ -2,8 +2,11 @@ import { defineStore } from "pinia";
 
 import getDrinks from "@/api/getDrinks";
 
+import { useUserStore } from "@/stores/user";
+
 export const FETCH_DRINKS = "FETCH_DRINKS";
 export const UNIQUE_CATEGORIES = "UNIQUE_CATEGORIES";
+export const FILTERED_DRINKS_BY_CATEGORIES = "FILTERED_DRINKS_BY_CATEGORIES";
 
 export const useDrinksStore = defineStore("drinks", {
   state: () => ({
@@ -20,6 +23,17 @@ export const useDrinksStore = defineStore("drinks", {
       const uniqueCategories = new Set();
       state.drinks.forEach((drink) => uniqueCategories.add(drink.category));
       return uniqueCategories;
+    },
+    [FILTERED_DRINKS_BY_CATEGORIES](state) {
+      const userStore = useUserStore();
+
+      if (userStore.selectedCategories.length === 0) {
+        return state.drinks;
+      }
+
+      return state.drinks.filter((drink) =>
+        userStore.selectedCategories.includes(drink.category)
+      );
     },
   },
 });
--- tests/unit/stores/drinks.test.js
+++ tests/unit/stores/drinks.test.js
@@ -2,6 +2,7 @@ import { createPinia, setActivePinia } from "pinia";
 import axios from "axios";
 
 import { useDrinksStore } from "@/stores/drinks";
+import { useUserStore } from "@/stores/user";
 
 vi.mock("axios");
 
@@ -48,6 +49,44 @@ describe("getters", () => {
       const result = store.UNIQUE_CATEGORIES;
 
       expect(result).toEqual(new Set(["Shot", "Shake"]));
+    });
+  });
+
+  describe("FILTERED_DRINKS_BY_CATEGORIES", () => {
+    it("identifies drinks that are associated with the given categories", () => {
+      const drinksStore = useDrinksStore();
+      drinksStore.drinks = [
+        { category: "Shot" },
+        { category: "Shake" },
+        { category: "Beer" },
+      ];
+      const userStore = useUserStore();
+      userStore.selectedCategories = ["Shot", "Beer"];
+
+      const result = drinksStore.FILTERED_DRINKS_BY_CATEGORIES;
+
+      expect(result).toEqual([{ category: "Shot" }, { category: "Beer" }]);
+    });
+
+    describe("when the user has not selected any categories", () => {
+      it("returns all jobs", () => {
+        const drinksStore = useDrinksStore();
+        drinksStore.drinks = [
+          { category: "Shot" },
+          { category: "Shake" },
+          { category: "Beer" },
+        ];
+        const userStore = useUserStore();
+        userStore.selectedCategories = [];
+
+        const result = drinksStore.FILTERED_DRINKS_BY_CATEGORIES;
+
+        expect(result).toEqual([
+          { category: "Shot" },
+          { category: "Shake" },
+          { category: "Beer" },
+        ]);
+      });
     });
   });
 });
