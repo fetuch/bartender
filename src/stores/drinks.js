8385adeabb61a20aeba329c30dd77a4e9337b268
Fetuch <rafal.nowak@punkgate.pl>
2023-01-22 07:41:24 +0100

    Add unique categories getter

--- src/stores/drinks.js
+++ src/stores/drinks.js
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 import getDrinks from "@/api/getDrinks";
 
 export const FETCH_DRINKS = "FETCH_DRINKS";
+export const UNIQUE_CATEGORIES = "UNIQUE_CATEGORIES";
 
 export const useDrinksStore = defineStore("drinks", {
   state: () => ({
@@ -12,6 +13,13 @@ export const useDrinksStore = defineStore("drinks", {
     async [FETCH_DRINKS]() {
       const drinks = await getDrinks();
       this.drinks = drinks;
+    },
+  },
+  getters: {
+    [UNIQUE_CATEGORIES](state) {
+      const uniqueCategories = new Set();
+      state.drinks.forEach((drink) => uniqueCategories.add(drink.category));
+      return uniqueCategories;
     },
   },
 });
