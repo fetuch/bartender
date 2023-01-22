b81874a41494cea8fa804afd35066e08fd0805ca
Fetuch <rafal.nowak@punkgate.pl>
2023-01-22 07:54:35 +0100

    Use categories getter in component

--- src/components/DrinkResults/DrinkFiltersSidebar/DrinkFiltersSidebarCategories.vue
+++ src/components/DrinkResults/DrinkFiltersSidebar/DrinkFiltersSidebarCategories.vue
@@ -3,24 +3,13 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="h-8 w-1/2">
-            <input id="Beer" type="checkbox" class="mr-3" />
-            <label for="Beer">Beer</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Ordinary Drink" type="checkbox" class="mr-3" />
-            <label for="Ordinary Drink">Ordinary Drink</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Shake" type="checkbox" class="mr-3" />
-            <label for="Shake">Shake</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="Shot" type="checkbox" class="mr-3" />
-            <label for="Shot">Shot</label>
+          <li
+            v-for="category in UNIQUE_CATEGORIES"
+            :key="category"
+            class="h-8 w-1/2"
+          >
+            <input :id="category" type="checkbox" class="mr-3" />
+            <label :for="category">{{ category }}</label>
           </li>
         </ul>
       </fieldset>
@@ -29,10 +18,16 @@
 </template>
 
 <script>
+import { mapState } from "pinia";
+import { useDrinksStore, UNIQUE_CATEGORIES } from "@/stores/drinks";
+
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
 
 export default {
   name: "DrinkFiltersSidebarCategories",
   components: { CollapsibleAccordion },
+  computed: {
+    ...mapState(useDrinksStore, [UNIQUE_CATEGORIES]),
+  },
 };
 </script>
