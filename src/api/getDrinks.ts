612dcf807fe81f392f399c198b4eac907eb4b9ea
Fetuch <rafal.nowak@punkgate.pl>
2023-01-29 08:51:17 +0100

    Use drink type

--- src/api/getDrinks.ts
+++ src/api/getDrinks.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+import type { Drink } from "@/api/types";
+
 const getDrinks = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL;
   const url = `${baseUrl}/drinks`;
-  const response = await axios.get(url);
+  const response = await axios.get<Drink[]>(url);
   return response.data;
 };
 
