eecc213f44eacb64f66ce55f0afc3c0297196498
Fetuch <rafal.nowak@punkgate.pl>
2023-01-29 09:05:42 +0100

    Add user state interface

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,8 +3,14 @@ import { defineStore } from "pinia";
 export const ADD_SELECTED_CATEGORIES = "ADD_SELECTED_CATEGORIES";
 export const ADD_SELECTED_GLASS_TYPES = "ADD_SELECTED_GLASS_TYPES";
 
+export interface UserState {
+  isLoggedIn: boolean;
+  selectedCategories: string[];
+  selectedGlassTypes: string[];
+}
+
 export const useUserStore = defineStore("user", {
-  state: () => ({
+  state: (): UserState => ({
     isLoggedIn: false,
     selectedCategories: [],
     selectedGlassTypes: [],
