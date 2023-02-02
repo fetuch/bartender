fe16e90300ad7a5de397befcebf77b1c8432e748
Rafal Nowak <rnowak@media4u.pl>
2023-02-02 08:21:18 +0100

    Connect name search input with store

--- src/components/DrinkResults/DrinkFiltersSidebar/DrinkFiltersSidebarName.vue
+++ src/components/DrinkResults/DrinkFiltersSidebar/DrinkFiltersSidebarName.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mt-2">
     <input
+      v-model.lazy.trim="nameSearchTerm"
       type="text"
       class="h-12 w-full rounded border border-solid border-brand-gray-1 p-3 text-base shadow-gray"
       placeholder="Margarita, Cuba Libre etc..."
@@ -9,18 +10,18 @@
 </template>
 
 <script lang="ts" setup>
-// import { computed } from "vue";
+import { computed } from "vue";
 
-// import { useUserStore } from "@/stores/user";
+import { useUserStore } from "@/stores/user";
 
-// const userStore = useUserStore();
+const userStore = useUserStore();
 
-// const nameSearchTerm = computed({
-//   get() {
-//     return userStore.nameSearchTerm;
-//   },
-//   set(value: string) {
-//     userStore.UPDATE_NAME_SEARCH_TERM(value);
-//   },
-// });
+const nameSearchTerm = computed({
+  get() {
+    return userStore.nameSearchTerm;
+  },
+  set(value: string) {
+    userStore.UPDATE_NAME_SEARCH_TERM(value);
+  },
+});
 </script>
